2c7baaef4ba35efea575f471e6b94175bf5944c6
ahmed abd elwahab <ahmed.almelegy617@el-eng.menofia.edu.eg>
2019-11-20 13:24:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -9,13 +9,13 @@ function teenager(age) {
     return "You are teenager!";
   }
   else if (age < 13){
-    return "You are not teenager!"
+    return "You are not a teenager!"
   }
 }
 
 function ageChecker(age) {
   if(age < 13){
-    return "You are a kid!";
+    return "You are a kid";
   }
   else if(age >= 13 && age <= 19){
     return "You are a teenager!"
