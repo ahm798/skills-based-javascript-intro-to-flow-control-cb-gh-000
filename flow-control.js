2ad5f172a2aa880978cb89f18af12d629dffa16f
ahmed abd elwahab <ahmed.almelegy617@el-eng.menofia.edu.eg>
2019-11-20 13:25:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,7 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if(age >= 13 && age <= 19){
-    return "You are teenager!";
+    return "You are a teenager!";
   }
   else if (age < 13){
     return "You are not a teenager"
